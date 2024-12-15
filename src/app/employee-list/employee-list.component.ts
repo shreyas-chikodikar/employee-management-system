dbcb2d9233036662cd6fa98fae2366f31876e2bb
shreyas-chikodikar <chikodikarshreyas98@gmail.com>
2024-12-15 17:02:48 +0530

    path updated

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Employee } from '../../model/employee.component';
+import { Employee } from '../../model/employee';
 import { Router } from '@angular/router';
 import { SharedDataService } from '../../services/shared-data.service';
 
